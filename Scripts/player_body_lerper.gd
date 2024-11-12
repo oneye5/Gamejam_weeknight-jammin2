6c8bbcef5e587b4f212636a0e40a0fbbbcb312c9
Owan Lazic <ocjlaz@gmail.com>
2024-11-13 11:03:33 +1300

    added adpative sync fixing

--- Scripts/player_body_lerper.gd
+++ Scripts/player_body_lerper.gd
@@ -1,6 +1,7 @@
 extends Node3D
-@export var pos_lerp_y : float = 0.5
-@export var pos_lerp_x : float = 0.02
+@export var pos_lerp_y : float = 0.3
+@export var pos_lerp_x : float = 0.01
+@export var acceptable_distance = 0.01
 var prev_pos : Vector3 = Vector3.ONE
 
 func _process(delta: float) -> void:
@@ -8,9 +9,13 @@ func _process(delta: float) -> void:
 	handle_rotate(delta)
 
 func lerp_pos() -> void:
-	position.x = lerp(position.x, prev_pos.x + 0.025 , pos_lerp_x) 
-	position.y = lerp(position.y, prev_pos.y , pos_lerp_y)
 	prev_pos = manager_singleton.instance().player.position
+	var xDist = abs(position.x - prev_pos.x)
+	var scale_factor = max(0.1,1.0 - (acceptable_distance / xDist)) * 2.0
+	print(xDist)
+	print("scale factor : " + str(scale_factor))
+	position.x = lerp(position.x, prev_pos.x + 0.08 , pos_lerp_x * scale_factor) 
+	position.y = lerp(position.y, prev_pos.y , pos_lerp_y)
 
 func handle_rotate(delta) -> void:
 	if manager_singleton.instance().player.down_raycast.is_colliding():
