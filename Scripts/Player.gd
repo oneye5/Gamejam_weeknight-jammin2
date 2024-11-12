da6da654158b2a86367d97501914960ff4e01c3d
Owan Lazic <ocjlaz@gmail.com>
2024-11-12 13:29:33 +1300

    working block placer

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,6 +8,7 @@ class_name Player
 @onready var down_raycast = $down_cast
 @onready var up_raycast = $up_cast
 @onready var hurtbox = $hurtbox
+@onready var children_parent = $"../child_bricks"
 
 
 var current_gravity : float = default_gravity
@@ -30,9 +31,28 @@ func _tick_die():
 	if hurtbox.is_colliding():
 		_player_died()
 
+func _place_block():
+	var grid_size_x = 0.08
+	var grid_size_y = 0.064
+	
+	var block_pos = position
+	#snap to grid
+	block_pos.x = round(block_pos.x / grid_size_x) * grid_size_x
+	block_pos.y = round(block_pos.y / grid_size_y) * grid_size_y
+	#offset
+	block_pos.y -= grid_size_y * 2
+	block_pos.x += grid_size_x * 2
+	var block = preload("res://Scenes/spawned_block.tscn")
+	var new_block = block.instantiate()
+	new_block.position = block_pos
+	children_parent.add_child(new_block)
+	
+
 func _tick_input():	
 	if Input.is_action_pressed("jump") and down_raycast.is_colliding(): #jump
 		linear_velocity.y = jump_vel
+	if Input.is_action_just_pressed("place"):
+		_place_block()
 
 var prev_x = null
 var expected_rate
